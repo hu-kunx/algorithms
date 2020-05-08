a392576757206f43163484a90ac30f9dd1ab5762
kun <36065805+kuunnnn@users.noreply.github.com>
2020-05-08 09:11:08 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,5 @@ module.exports = {
   },
   // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testRegex: './src/.*.test.ts$',
-  moduleFileExtensions: ['ts'],
+  moduleFileExtensions: ['ts',"js"],
 };
