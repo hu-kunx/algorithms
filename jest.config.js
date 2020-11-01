a437005c96f34dc203e4d5e310dc99454869d67b
胡坤 <mrhukun@gmail.com>
2020-11-01 22:49:59 +0800

    refactor: 目录结构调整

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 module.exports = {
   transform: {
-    // '^.+\\.tsx?$': '<rootDir>/node_modules/babel-jest',
     '^.+\\.ts$': 'ts-jest',
   },
-  // testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   testRegex: './test/.*.test.ts$',
   moduleFileExtensions: ['ts', 'js'],
 };
